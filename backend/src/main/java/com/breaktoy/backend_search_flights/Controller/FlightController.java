63be66cad4cc8edeb7432b44636cb38338cec421
SczSca <antonio.alvarez@encora.com>
2024-11-05 21:01:40 -0600

    Flight Controller

---
+++ backend/src/main/java/com/breaktoy/backend_search_flights/Controller/FlightController.java
@@ -0,0 +1,26 @@
+package com.breaktoy.backend_search_flights.Controller;
+
+import com.breaktoy.backend_search_flights.Entity.IATAItem;
+import com.breaktoy.backend_search_flights.Model.AccessTokenResponse;
+import com.breaktoy.backend_search_flights.Service.FlightService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/flights")
+@RequiredArgsConstructor
+public class FlightController {
+    private final FlightService flightService;
+
+    @GetMapping("/test/IATA")
+    public ResponseEntity<List<IATAItem>> getIATARecommendations(
+        @RequestParam(value = "keyword", required = true) String keyword
+    ){
+        return flightService.getIATAsRecommendations(keyword);
+    }
+
+}
