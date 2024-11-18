13d30077d4c9b4cc4b4c2d1fe7015fefeab59e46
SczSca <antonio.alvarez@encora.com>
2024-11-18 14:30:42 -0600

    using Localization Provider, Browser Routes and two custom context provider

--- front/search-flights/src/main.tsx
+++ front/search-flights/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { SearchFlightProvider } from "./context/SearchFlightContext.tsx";
+import { APIConsumerProvider } from "./context/APIConsumerContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <APIConsumerProvider>
+          <SearchFlightProvider>
+            <App />
+          </SearchFlightProvider>
+        </APIConsumerProvider>
+      </LocalizationProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
