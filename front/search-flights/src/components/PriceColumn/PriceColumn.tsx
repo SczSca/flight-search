a6d49fca3046bb8f831554767774c0842fca71a7
SczSca <antonio.alvarez@encora.com>
2024-11-25 17:07:37 -0600

    pricecolumn component changes

--- front/search-flights/src/components/PriceColumn/PriceColumn.tsx
+++ front/search-flights/src/components/PriceColumn/PriceColumn.tsx
@@ -1,6 +1,7 @@
 import { Box, Divider, Typography } from "@mui/material";
-import { currencyValues, FOPrice, TravelerPricings } from "../../types";
+import { currencyValues, FOPrice } from "../../types";
 import { currencySymbols } from "../../utils";
+import { Fragment } from "react";
 
 interface Props {
   price: FOPrice;
@@ -9,82 +10,109 @@ interface Props {
     travelerType: string;
     price: {
       total: string;
-      currency: currencyValues;
+      currency: string;
       base: string;
     };
   }[];
 }
 export const PriceColumn = ({ price, travelerPricings }: Props) => {
-  const currencyStr = `${price.currency}${currencySymbols[price.currency]}`;
+  const currency = price.currency as currencyValues;
+
+  const currencyStr = `${price.currency}${currencySymbols[currency]}`;
 
   const paddingLeftMain = 2;
   const paddingLeftTravelerPrice = 2.5;
 
   const renderFees: () => JSX.Element[] = () => {
-    return price.fees.map((fee) => {
+    return price.fees.map((fee, feeIdx) => {
       return (
-        <>
-          <Typography variant="body1" pl={paddingLeftTravelerPrice}>
+        <Fragment key={`fragment-fee-${feeIdx}`}>
+          <Typography
+            variant="body1"
+            pl={paddingLeftTravelerPrice}
+            key={`fee-info-${feeIdx}`}
+          >
             {fee.type}: {currencyStr}
             {fee.amount}
           </Typography>
-        </>
+        </Fragment>
       );
     });
   };
   const renderTravelerPrices: () => JSX.Element[] = () => {
-    return travelerPricings.map((traveler) => {
+    return travelerPricings.map((traveler, travelerIdx) => {
       return (
-        <>
-          <Box pl={paddingLeftMain}>
-            <Typography variant="subtitle1" component="h3" pl={1}>
+        <Fragment key={`fragment-traveler-price-${travelerIdx}`}>
+          <Box pl={paddingLeftMain} key={`box-traveler-price-${travelerIdx}`}>
+            <Typography
+              variant="subtitle1"
+              component="h3"
+              pl={1}
+              key={`traveler-type-${travelerIdx}`}
+            >
               Traveler:
               {traveler.travelerId} {traveler.travelerType}
             </Typography>
-            <Typography variant="body1" pl={paddingLeftTravelerPrice}>
+            <Typography
+              variant="body1"
+              pl={paddingLeftTravelerPrice}
+              key={`traveler-base-price-${travelerIdx}`}
+            >
               Base: {currencyStr}
               {traveler.price.base}
             </Typography>
-            <Typography variant="body1" pl={paddingLeftTravelerPrice}>
+            <Typography
+              variant="body1"
+              pl={paddingLeftTravelerPrice}
+              key={`traveler-total-price-${travelerIdx}`}
+            >
               Total: {currencyStr}
               {traveler.price.total}
             </Typography>
           </Box>
-          <Divider sx={{ mb: 1, mt: 1 }} />
-        </>
+          <Divider
+            sx={{ mb: 1, mt: 1 }}
+            key={`divider-traveler-price-${travelerIdx}`}
+          />
+        </Fragment>
       );
     });
   };
 
   return (
     <>
-      <Box pl={paddingLeftMain}>
+      <Box pl={paddingLeftMain} data-testid="price-column">
         <Typography variant="h5" component="h2">
           Price breakdown
         </Typography>
-        <Typography variant="body1" pl={paddingLeftTravelerPrice}>
-          Base: {currencyStr}
-          {price.base}
-        </Typography>
-        <Typography
-          variant="body1"
-          fontWeight={600}
-          fontSize={20}
-          pl={paddingLeftTravelerPrice}
-        >
-          Total: {currencyStr}
-          {price.grandTotal}
-        </Typography>
-        <Typography variant="h6" component="h3">
-          Fees:
-        </Typography>
-        {renderFees()}
         <Typography variant="h6" component="h3">
           Travelers:
         </Typography>
       </Box>
       <Divider />
       {renderTravelerPrices()}
+      <Box pl={paddingLeftMain}>
+        <Typography variant="body1" pl={paddingLeftTravelerPrice}>
+          Total Base: {currencyStr}
+          {price.base}
+        </Typography>
+
+        <Typography variant="h6" component="h3">
+          Fees:
+        </Typography>
+
+        {renderFees()}
+        <Typography
+          variant="body1"
+          fontWeight={600}
+          fontSize={20}
+          mt={1}
+          mb={2}
+        >
+          Grand Total: {currencyStr}
+          {price.grandTotal}
+        </Typography>
+      </Box>
     </>
   );
 };
