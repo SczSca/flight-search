5ea51b65f9953de7655069c45ed95721a7368ec4
SczSca <antonio.alvarez@encora.com>
2024-11-25 17:10:58 -0600

    change methods names and add new methods

--- backend/src/main/java/com/breaktoy/backend_search_flights/Repository/FlightRepository.java
+++ backend/src/main/java/com/breaktoy/backend_search_flights/Repository/FlightRepository.java
@@ -1,13 +1,15 @@
 package com.breaktoy.backend_search_flights.Repository;
 
 import com.breaktoy.backend_search_flights.Entity.AccessToken;
+import com.breaktoy.backend_search_flights.Entity.IATAItem;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface FlightRepository {
-
-    Optional<String> findIATAsCode(String keyword);
-    Void setAccessTokenObj(String newAccessTokenStr, LocalDateTime newExpirationDate);
+    void saveAirportInfo(String IATACode, IATAItem iataItem);
+    Optional<IATAItem> findIATAItem(String iataCode);
+    Optional<String> getAirportName(String iataCode);
+    void setAccessTokenObj(String newAccessTokenStr, LocalDateTime newExpirationDate);
     AccessToken getAccessTokenObj();
 }
