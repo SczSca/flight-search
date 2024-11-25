c3d616104f1c044145b4a2a6cf547ee2d5b3762e
SczSca <antonio.alvarez@encora.com>
2024-11-25 17:06:31 -0600

    groupbuttonfilter component changes

--- front/search-flights/src/components/GroupButtonFilter/GroupButtonFilter.tsx
+++ front/search-flights/src/components/GroupButtonFilter/GroupButtonFilter.tsx
@@ -1,5 +1,4 @@
 import { Button, ButtonGroup } from "@mui/material";
-import React from "react";
 
 interface Props {
   btnTextsArr: string[];
@@ -10,7 +9,11 @@ export const GroupButtonFilter = ({ btnTextsArr, onClickHandlers }: Props) => {
   return (
     <ButtonGroup variant="outlined" aria-label="Basic button group" fullWidth>
       {btnTextsArr.map((text, idx) => (
-        <Button key={idx} onClick={onClickHandlers[idx]}>
+        <Button
+          key={idx}
+          onClick={onClickHandlers[idx]}
+          data-testid={`button-filter-${idx}`}
+        >
           {text}
         </Button>
       ))}
