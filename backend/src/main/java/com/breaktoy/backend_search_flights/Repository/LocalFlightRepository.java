edfd9fb0470e40bbb13c6967d1306790eb6ba51b
SczSca <antonio.alvarez@encora.com>
2024-11-25 17:12:26 -0600

    add airportInfo storage for iatacodes

--- backend/src/main/java/com/breaktoy/backend_search_flights/Repository/LocalFlightRepository.java
+++ backend/src/main/java/com/breaktoy/backend_search_flights/Repository/LocalFlightRepository.java
@@ -1,9 +1,14 @@
 package com.breaktoy.backend_search_flights.Repository;
 
 import com.breaktoy.backend_search_flights.Entity.AccessToken;
+import com.breaktoy.backend_search_flights.Entity.Destination;
+import com.breaktoy.backend_search_flights.Entity.IATAItem;
+import com.breaktoy.backend_search_flights.Model.API.FlightOffersResponse.FlightOffersResponse;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,26 +17,56 @@ public class LocalFlightRepository implements FlightRepository{
                 .accessToken("")
                 .expirationDate(LocalDateTime.now())
                 .build();
+    private final HashMap<String, Destination> flights = new HashMap<>();
+    private final HashMap<String, IATAItem> airportInfo = new HashMap<>();
+
+    public void saveAirportInfo(String IATACode, IATAItem iataItem){
+        airportInfo.put(IATACode,iataItem);
+    }
+
+    public void saveAllIATAItems(List<IATAItem> iataItems){
+        for(IATAItem iataItem: iataItems){
+            String iataCode = iataItem.getIataCode();
+
+            if(findIATAItem(iataCode).isEmpty()){
+                saveAirportInfo(iataCode,iataItem);
+            }
+        }
+    }
+
+    public String saveFlightResult(String originLocationCode,String destinationLocationCode, FlightOffersResponse flightResults){
+        HashMap<String, FlightOffersResponse> flightResultsMap = new HashMap<>();
+        flightResultsMap.put(destinationLocationCode,flightResults);
+
+        Destination destination = Destination.builder()
+                .flightResults(flightResultsMap)
+                .build();
+
+        flights.put(originLocationCode,destination);
+        return "0";
+    }
+
 
 
     @Override
-    public Void setAccessTokenObj(String newAccessTokenStr, LocalDateTime newExpirationDate) {
-//        AccessToken newAccessToken = AccessToken.builder()
-//                .accessToken(newAccessTokenStr)
-//                .expirationDate(newExpirationDate)
-//                .build();
-//        accessTokenObj = newAccessToken;
+    public void setAccessTokenObj(String newAccessTokenStr, LocalDateTime newExpirationDate) {
         accessTokenObj.setAccessToken(newAccessTokenStr);
         accessTokenObj.setExpirationDate(newExpirationDate);
-        return null;
     }
 
     @Override
     public AccessToken getAccessTokenObj() {
         return accessTokenObj;
     }
+
     @Override
-    public Optional<String> findIATAsCode(String keyword) {
-        return Optional.empty();
+    public Optional<IATAItem> findIATAItem(String iataCode) {
+        return Optional.ofNullable(airportInfo.get(iataCode));
+    }
+
+    @Override
+    public Optional<String> getAirportName(String iataCode){
+        Optional<IATAItem> iataItem = findIATAItem(iataCode);
+        return iataItem.map(IATAItem::getDetailedName);
     }
 }
