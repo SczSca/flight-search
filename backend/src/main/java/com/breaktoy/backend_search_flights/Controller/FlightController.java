e8d9612c2adfba28b9d5bf6be282c2e4e1935d6b
SczSca <antonio.alvarez@encora.com>
2024-11-25 17:10:27 -0600

    add cross origin and new query param

--- backend/src/main/java/com/breaktoy/backend_search_flights/Controller/FlightController.java
+++ backend/src/main/java/com/breaktoy/backend_search_flights/Controller/FlightController.java
@@ -1,14 +1,14 @@
 package com.breaktoy.backend_search_flights.Controller;
 
-import com.breaktoy.backend_search_flights.Entity.IATAItem;
+
 import com.breaktoy.backend_search_flights.Model.API.FlightOffersResponse.FlightOffersResponse;
+import com.breaktoy.backend_search_flights.Model.API.IATAResponse;
 import com.breaktoy.backend_search_flights.Service.FlightService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -16,7 +16,7 @@ public class FlightController {
     private final FlightService flightService;
 
     @GetMapping("/info/IATA")
-    public ResponseEntity<List<IATAItem>> getIATARecommendations(
+    public ResponseEntity<IATAResponse> getIATARecommendations(
         @RequestParam(value = "keyword", required = true) String keyword
     ){
         return flightService.returnIATAsRecommendations(keyword);
@@ -30,9 +30,10 @@ public class FlightController {
             @RequestParam(value = "returnDate", required = true) String returnDate,
             @RequestParam(value = "adults", required = true) Integer adults,
             @RequestParam(value = "nonStop", required = true) Boolean nonStop,
-            @RequestParam(value = "currencyCode", required = true) String currencyCode
+            @RequestParam(value = "currencyCode", required = true) String currencyCode,
+            @RequestParam(value = "order", required = true) String order
     ){
-        return flightService.returnFlightOffers(originLocationCode,destinationLocationCode,departureDate,returnDate,adults,nonStop,currencyCode);
+        return flightService.returnFlightOffers(originLocationCode,destinationLocationCode,departureDate,returnDate,adults,nonStop,currencyCode, order);
     }
 
 }
